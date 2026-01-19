4900780d6e5560857cbf35a276ce5282614d1c74
Iced0368 <iced03682@gmail.com>
2026-01-19 22:03:08 +0900

    [FEAT] config.jsx 로컬 스토리지 상수 추가

--- src/core/constants/config.jsx
+++ src/core/constants/config.jsx
@@ -1,2 +1,10 @@
-export const FAVORITE_PACKAGE_ID = -1;
-export const SERACH_PACKAGE_ID = -2;
+export const RECENT_PACKAGE_ID = "0";
+export const FAVORITE_PACKAGE_ID = "-1";
+export const SERACH_PACKAGE_ID = "-2";
+
+export const LOCAL_STORAGE_ARCACON_DATA = "arcacon-data";
+export const LOCAL_STORAGE_FAVORITE_DATA = "arcacon-favorites";
+export const LOCAL_STORAGE_MEMO_DATA = "arcacon-memos";
+export const LOCAL_STORAGE_RECENT_DATA = "arcacon-recents";
+
+export const MAX_RECENT_ITEMS = 50;
