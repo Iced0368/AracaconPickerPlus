c31e83d18c56fa6245493f2b0e4b20b6c75c4bc8
Iced0368 <iced03682@gmail.com>
2026-01-21 15:11:56 +0900

    [REFAC] SearchController에서 클릭 이벤트 처리 개선

--- src/modules/search/controller.jsx
+++ src/modules/search/controller.jsx
@@ -7,6 +7,17 @@ export default function SearchController({ setInputValue, setKeyword }) {
       const uid = target.closest(".arcaconPicker")?.getAttribute("data-uid");
       setKeyword(uid, "");
       setInputValue(uid, "");
+
+      setTimeout(() => {
+        const t = e.target;
+        const n = t.getAttribute("data-package-id");
+        const a = t
+          .closest(".arcaconPicker")
+          .querySelector(
+            `.content [data-package-id="${n}"].package-wrap, .content [data-package-id="${n}"].--package-wrap`,
+          );
+        a && (a.parentElement.scrollTop = a.offsetTop);
+      }, 100);
     }
   });
 
