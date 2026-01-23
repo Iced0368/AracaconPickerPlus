288df304bb94e9ab30cad7c30f712493176ce795
Iced0368 <iced03682@gmail.com>
2026-01-23 17:56:18 +0900

    [REFAC] MemoController에서 불필요한 useRef 및 롱프레스 이벤트 제거

--- src/modules/memo/controller.jsx
+++ src/modules/memo/controller.jsx
@@ -1,11 +1,6 @@
-import { useEffect, useRef } from "react";
 import { useEventListener } from "../../hooks";
-import { useMemoStore } from "../../stores";
 
 export default function MemoController({ openMemo }) {
-  const timerRef = useRef(null);
-  const longPressEventRef = useRef(null);
-
   // .thumbnail-wrap에서 우클릭
   useEventListener(
     "contextmenu",
@@ -16,35 +11,6 @@ export default function MemoController({ openMemo }) {
 
       const id = target.getAttribute("data-attachment-id");
       openMemo(id);
-    },
-    document,
-    true,
-  );
-
-  // 터치 롱프레스 (모바일)
-  useEventListener(
-    "pointerdown",
-    (e) => {
-      const target = e.target.closest(".thumbnail-wrap");
-      if (!target) return;
-      timerRef.current = setTimeout(() => {
-        longPressEventRef.current = e;
-        const id = target.getAttribute("data-attachment-id");
-        openMemo(id);
-      }, 500);
-    },
-    document,
-    true,
-  );
-  useEventListener(
-    "pointerup",
-    (e) => {
-      clearTimeout(timerRef.current);
-      if (longPressEventRef.current) {
-        e.preventDefault();
-        e.stopPropagation();
-        longPressEventRef.current = null;
-      }
     },
     document,
     true,
