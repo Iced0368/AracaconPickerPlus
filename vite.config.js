85e99f433dc0b9fba4482cab82d609216ae855ac
Iced0368 <iced03682@gmail.com>
2026-03-02 00:08:43 +0900

    [UPD] 버전 1.1.1에서 1.1.2로 업데이트

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
         match: ["https://arca.live/*"],
         description: "아카콘 기능 확장",
         license: "MIT",
-        version: "1.1.1",
+        version: "1.1.2",
         "run-at": "document-start",
       },
     }),
